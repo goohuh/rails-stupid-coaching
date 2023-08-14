363f476734ce18a6caa1154355da9366c74c5212
Arnaud Legaignoux <arnaud.legaignoux@gmail.com>
2023-08-14 12:50:27 +0200

    ok

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @question = params[:question]
+    @answer = if @question.include? '?'
+                'Silly question, get dressed and go to work!'
+              elsif @question == 'I am going to work right now!'
+                ''
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
+  end
+end
